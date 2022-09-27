11d568595411f19c5cb7dbed3e0132336c86ee23
Lucas Soares <75669462+soareslp@users.noreply.github.com>
2022-09-26 21:31:55 -0300

    att

---
+++ aula13/aula13.js
@@ -0,0 +1,10 @@
+var c = 1
+/*while (c<11){
+    console.log(`${c}° Tudo bem?`)
+    c++
+}*/
+
+do {
+    console.log(`${c}° Tudo bem mermão??`)
+    c++
+} while(c < 6)
---
+++ aula14/aula14.js
@@ -0,0 +1,5 @@
+console.log(`Vai começar...`)
+for (c=1; c<=10; c++){
+    console.log(`${c}° Tudo bem mano?!`)
+}
+console.log(`FIM!`)
