5ffc5d94c6a75eb3fc146af3074b6d56847e862a
Lucas Soares <75669462+soareslp@users.noreply.github.com>
2022-11-22 20:46:00 -0300

    aluramidi

---
+++ alura/aluramidi/main.js
@@ -0,0 +1,30 @@
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio).play();
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    alert('Elemento não encontrado');
+  }
+}
+
+const listaTeclas = document.querySelectorAll(".tecla");
+
+for (let contador = 0; contador < listaTeclas.length; contador++) {
+  const tecla = listaTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter")
+      tecla.classList.add("ativa");
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
