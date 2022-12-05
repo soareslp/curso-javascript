1d79e0cb37a92c0e6e88a80e63ea5e559b380747
Lucas Soares <75669462+soareslp@users.noreply.github.com>
2022-12-05 14:24:06 -0300

    att

--- alura/alurabooks2.0(metodos)/app/metodoMap.js
+++ alura/alurabooks2.0(metodos)/app/metodoMap.js
@@ -1,6 +1,6 @@
 function aplicarDesconto(livros) {
     const desconto = 0.3
-    livrosComDesconto = livros.map(livro => {
+    const livrosComDesconto = livros.map(livro => {
         return {...livro, preco: livro.preco - (livro.preco * desconto)}
     })
     return livrosComDesconto
