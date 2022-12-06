eacd83ee66fc6a4200f21d6fef6cd6bd5e4c80d2
Lucas Soares <75669462+soareslp@users.noreply.github.com>
2022-12-05 21:14:08 -0300

    jogo do chute por voz

---
+++ alura/numerosecreto/app/validacao.js
@@ -0,0 +1,45 @@
+function verificaSeOChutePossuiValorValido(chute) {
+    const numero =+ chute;
+
+    if (chuteForInvalido(numero)) {
+       elementoChute.innerHTML += '<div>Chute inválido. Fale um número</div>'
+       return
+    }
+
+    if (numeroMaiorOuMenorQuePermitido(numero)) {
+        elementoChute.innerHTML += `<div>Chute fora do limite. Deve estar entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h1>Parabéns! Você acertou!!</h1>
+        <h3>O número secreto foi ${numeroSecreto}</h3>
+        
+        <button id="reiniciar" class="btnJogar">Jogar Novamente</button>`
+    }else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += 
+        `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    }else {
+        elementoChute.innerHTML += 
+        `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
+    }
+}
+
+function chuteForInvalido(numero){
+    return Number.isNaN(numero)
+}
+
+function numeroMaiorOuMenorQuePermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == "reiniciar") {
+        window.location.reload()
+    }
+})
+
+recognition.addEventListener('end', () => recognition.start())
